17cf49a8bd2aef7c803df4fe3241910a115c929a
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-02 19:48:28 +0100

    Added seccond test for Info component

--- src/components/Info/Info.test.js
+++ src/components/Info/Info.test.js
@@ -2,16 +2,31 @@ import { render, screen } from "@testing-library/react";
 import Info from "./Info";
 
 describe("Given Info component", () => {
-  describe("When it recieves the number display as true", () => {
+  describe("When it recieves display as true", () => {
     test("Then it should render a component with the text 'Calling...'", () => {
       const text = "Calling...";
       const display = true;
 
       render(<Info display={display} />);
 
-      const numberElement = screen.queryByText(text);
+      const infoElement = screen.queryByText(text);
 
-      expect(numberElement).toBeInTheDocument();
+      expect(infoElement).toBeInTheDocument();
+    });
+  });
+
+  describe("When it recieves display as false and deactivatedClass as 'off'", () => {
+    test("Then it should render a component with className'message off'", () => {
+      const deactivatedClass = "off";
+      const text = "Calling...";
+      const display = false;
+      const expectedClassName = "message off";
+
+      render(<Info display={display} deactivatedClass={deactivatedClass} />);
+
+      const infoElement = screen.queryByText(text);
+
+      expect(infoElement.className).toBe(expectedClassName);
     });
   });
 });
