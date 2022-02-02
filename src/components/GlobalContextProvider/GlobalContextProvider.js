ba110d136c417b083bde963c780551bff39887e6
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-02 21:42:41 +0100

    Created Keyboard.js

--- src/components/GlobalContextProvider/GlobalContextProvider.js
+++ src/components/GlobalContextProvider/GlobalContextProvider.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import globalContext from "../../contexts/globalContext";
 
 const GlobalContextProvider = ({ children }) => {
