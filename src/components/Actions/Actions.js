28dbe493b331ceb2d1d78f5047add6e648f32ddc
Pau Ibáñez <pauibanez2001@gmail.com>
2022-02-02 20:14:10 +0100

    Added logic to ether display call or hang

--- src/components/Actions/Actions.js
+++ src/components/Actions/Actions.js
@@ -1,18 +1,18 @@
 import Display from "../Display/Display";
 
-const Actions = () => {
+const Actions = ({ calling }) => {
   return (
     <div className="actions">
       <Display />
-      {/* <!-- El botón de llamar debe tener la clase "activo" cuando --> */}
-      {/* <!-- el número de teléfono tiene 9 cifras --> */}
-      <a href="asd" className="call">
-        Call
-      </a>
-      {/* <!-- Sólo se tiene que ver un botón u otro --> */}
-      <a href="asd" className="hang active">
-        Hang
-      </a>
+      {calling ? (
+        <a href="asd" className="hang active">
+          Hang
+        </a>
+      ) : (
+        <a href="asd" className="call">
+          Call
+        </a>
+      )}
     </div>
   );
 };
